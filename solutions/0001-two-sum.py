f030207aae4ea0f1bed7beb24d0e2f4d9fd31b86
thecoonbre-devoo <heqjeef685239@outlook.com>
2026-08-06 18:00:22 +0800

    Add solutions/0001-two-sum.py

---
+++ solutions/0001-two-sum.py
@@ -0,0 +1,10 @@
+"1. Two Sum"
+from typing import List
+class Solution:
+    def twoSum(self,nums:List[int],target:int)->List[int]:
+        seen={}
+        for i,n in enumerate(nums):
+            c=target-n
+            if c in seen:return[seen[c],i]
+            seen[n]=i
+        return[]
