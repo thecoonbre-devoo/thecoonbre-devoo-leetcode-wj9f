1441e30a133df6c702533c58672ea7f876daa33f
thecoonbre-devoo <heqjeef685239@outlook.com>
2026-08-06 18:00:26 +0800

    Add solutions/test_solutions.py

---
+++ solutions/test_solutions.py
@@ -0,0 +1,5 @@
+from solutions_0001_two_sum import Solution as S1
+from solutions_0009_palindrome import Solution as S9
+def test_1():assert S1().twoSum([2,7,11,15],9)==[0,1]
+def test_9():assert S9().isPalindrome(121)
+print("OK")
